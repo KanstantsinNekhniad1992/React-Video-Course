6cf0fcb708c682c6b9d5b53f1b72658c81a99605
Kanstantsin Nekhniadovich <kanstantsin_nekhniadovich@epam.com>
2016-12-30 17:44:30 +0300

    first commit: stop on 5 module

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+
+class CoursesPage extends React.Component {
+
+	render() {
+
+		return (
+			<div>
+				<h1>Courses</h1>
+			</div>
+		);
+
+	}
+
+}
+
+export default CoursesPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component {
+
+	render() {
+
+		return (
+			<div className="jumbotron">
+				<h1>Pluralsight Course</h1>
+				<p>React, Redux and react Router</p>
+				<Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+			</div>
+		);
+	}
+}
+
+export default HomePage;
